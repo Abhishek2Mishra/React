271534b61afff53929e2eaa34eefbe1884b1e376
abhishek <asmi9574@gmail.com>
2023-10-10 21:53:10 +0530

    first commit

---
+++ firstclass.js
@@ -0,0 +1,9 @@
+console.log("abhhishek")
+const acc = 123;
+const accc = 'ans';
+let ans = 567;
+let anstw = 'abhih';
+console.table([anstw, ans,acc,accc]);
+console.log(ans,anstw,accc,acc);
+console.log(typeof ans);
+console.log(typeof null);
